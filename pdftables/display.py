94c8dbe12837581f6dfed0f8c64d58eacaaba30f
Paul Furley <paul@paulfurley.com>
2013-06-27 15:38:23 +0100

    Tweaked the display output format to include column numbers and table dimensions

--- pdftables/display.py
+++ pdftables/display.py
@@ -1,26 +1,48 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals
 from collections import defaultdict
+from StringIO import StringIO
 
 
 def to_string(table):
     """
     Returns a list of the maximum width for each column across all rows
-    >>> to_string([['foo', 'goodbye'], ['llama', 'bar']])
-    None
+    >>> type(to_string([['foo', 'goodbye'], ['llama', 'bar']]))
+    <type 'unicode'>
     """
-    result = ''
+    result = StringIO()
+
+    (columns, rows) = get_dimensions(table)
+    result.write("     {} columns, {} rows\n".format(columns, rows))
     col_widths = find_column_widths(table)
     table_width = sum(col_widths) + len(col_widths) + 2
-    hbar = '     {}\n'.format('-' * table_width)
-    result += hbar
+    hbar = '    {}\n'.format('-' * table_width)
+
+    result.write("      {}\n".format(' '.join(
+        [unicode(col_index).rjust(width, ' ') for (col_index, width)
+         in enumerate(col_widths)])))
+
+    result.write(hbar)
     for row_index, row in enumerate(table):
         cells = [cell.rjust(width, ' ') for (cell, width)
                  in zip(row, col_widths)]
-        result += "{:>3}: | {}|\n".format(row_index, '|'.join(cells))
-    result += hbar
+        result.write("{:>3} | {}|\n".format(row_index, '|'.join(cells)))
+    result.write(hbar)
+    result.seek(0)
+    return unicode(result.read())
 
-    return result
+
+def get_dimensions(table):
+    """
+    Returns columns, rows for a table.
+    >>> get_dimensions([['row1', 'apple', 'llama'], ['row2', 'plum', 'goat']])
+    (3, 2)
+    >>> get_dimensions([['row1', 'apple', 'llama'], ['row2', 'banana']])
+    (3, 2)
+    """
+    rows = len(table)
+    cols = max(len(row) for row in table)
+    return (cols, rows)
 
 
 def find_column_widths(table):

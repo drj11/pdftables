25198c4f3fdd343e3a824b2b09d8661b10b60244
Ian Hopkinson <ian@scraperwiki.com>
2013-07-03 15:01:41 +0100

    Tidied up the analysis code so it runs with latest pdftables

--- pdftables/pdftables_analysis.py
+++ pdftables/pdftables_analysis.py
@@ -16,7 +16,7 @@ Analysis and visualisation library for pdftables
 import pdftables as pt
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-
+from tree import Leaf, LeafList
 
 
 FilterOptions = ['LTPage','LTTextBoxHorizontal','LTFigure','LTLine','LTRect','LTImage','LTTextLineHorizontal','LTCurve', 'LTChar', 'LTAnon']
@@ -54,38 +54,32 @@ def plotpage(d):
         ax1.plot([box[0],box[2],box[2],box[0],box[0]],[box[1],box[1],box[3],box[3],box[1]],color = thiscolour )
 
     # fig.suptitle(title, fontsize=15)
-
     divider = make_axes_locatable(ax1)
-    axHistx = divider.append_axes("top", 1.2, pad=0.1, sharex=ax1)
-    axHisty = divider.append_axes("left", 1.2, pad=0.1, sharey=ax1)
+    #plt.setp(ax1.get_yticklabels(),visible=False)
+    ax1.yaxis.set_label_position("right")
 
-    plt.setp(ax1.get_yticklabels(),visible=False)
-
-    axHistx.plot(map(float,d.top_plot.keys()),map(float,d.top_plot.values()), color = 'red')
-    #axHistx.scatter(map(float,xhistright.keys()),map(float,xhistright.values()), color = 'red')
-    axHisty.plot(map(float,d.left_plot.values()),map(float,d.left_plot.keys()), color = 'red')
-    #axHisty.scatter(map(float,yhistbottom.values()),map(float,yhistbottom.keys()), color = 'red')
-    #fig.suptitle('%s : Page %d' % (SelectedPDF,pagenumber), fontsize=15)
-    # plt.draw()
-
+    if d.top_plot: 
+        axHistx = divider.append_axes("top", 1.2, pad=0.1, sharex=ax1)
+        axHistx.plot(map(float,d.top_plot.keys()),map(float,d.top_plot.values()), color = 'red')
+        
+    if d.left_plot:
+        axHisty = divider.append_axes("left", 1.2, pad=0.1, sharey=ax1)
+        axHisty.plot(map(float,d.left_plot.values()),map(float,d.left_plot.keys()), color = 'red')
+    
     if d.y_comb:
         miny = min(d.y_comb)
         maxy = max(d.y_comb)
 
         for x in d.x_comb:
             ax1.plot([x,x],[miny,maxy],color = "black")
-            #axHistx.plot([x,x],[0,35],color = "black")
             axHistx.scatter(x,0,color = "black")
 
     if d.x_comb:
-        #minx = min([box.left for box in d.BoxList])
-        #maxx = max([box.right for box in d.BoxList])
         minx = min(d.x_comb)
         maxx = max(d.x_comb)
 
         for y in d.y_comb:
             ax1.plot([minx,maxx],[y,y],color = "black")
-            #axHisty.plot([0,8],[y,y],color = "black")
             axHisty.scatter(1,y,color = "black")
 
     plt.draw()
@@ -103,34 +97,43 @@ def plothistogram(hist):
     return fig
 
 def plotAllPages(fh):
-    tol = 5 # This is the tolerance for histogram rounding
-    doc, interpreter, device = pt.InitializePDFminer(fh)
-    print SelectedPDF
+    #tol = 5 # This is the tolerance for histogram rounding
+    
+    fig_list = []
+    ax1_list = []
+    
+    doc, interpreter, device = pt.initialize_pdf_miner(fh)
+    # print SelectedPDF
     Creator = doc.info[0]['Creator']
     print "Created by: %s" % Creator
     #flt = 'LTTextLineHorizontal'
-    flt = ['LTPage','LTTextLineHorizontal']
+    #flt = ['LTPage','LTTextLineHorizontal']
+    flt = ['LTPage','LTFigure','LTLine','LTRect','LTImage','LTTextLineHorizontal','LTCurve']
     for i,page in enumerate(doc.get_pages()):
         # page = next(doc.get_pages())
 
         interpreter.process_page(page)
     # receive the LTPage object for the page.
         layout = device.get_result()
-        BoxList, ElementCount = pt.getbboxes(layout)
+        box_list = LeafList().populate(layout, interested = flt)
 
-        ModalHeight = pt.calcModalHeight(BoxList)
-        # index 0 = left, 1 = top, 2 = right, 3 = bottom
-        xhistleft = pt.histogramFromBoxList(BoxList,flt,LEFT,tol)
-        xhistright = pt.histogramFromBoxList(BoxList,flt,RIGHT,tol)
-        yhisttop = pt.histogramFromBoxList(BoxList,flt,TOP,tol)
-        yhistbottom = pt.histogramFromBoxList(BoxList,flt,BOTTOM,tol)
-
-        title = "%s : page %d" %(SelectedPDF,i+1)
-        plotpage(BoxList,xhistleft,xhistright,yhisttop, yhistbottom, title,flt)
-        print "Page %d" % (i+1), ElementCount
+        ModalHeight = pt.calculate_modal_height(box_list)
+        
+        diagnostic_data = pt.TableDiagnosticData(
+                box_list,
+                {},
+                {},
+                [],
+                [])
+        
+        fig, ax1 = plotpage(diagnostic_data)
+        fig_list.append(fig)
+        ax1_list.append(ax1)
+        
+        title = "page %d" % (i+1)
+        fig.suptitle(title)
+        #print "Page %d" % (i+1), ElementCount
+        print box_list.count()
         print "Modal character height: %d" % ModalHeight
-        # plothistogram(xhist)
-        # plothistogram(yhist)
-        plt.show()
 
-    return BoxList
+    return fig_list, ax1_list

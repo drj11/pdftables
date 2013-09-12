f4777945ddef5128ce597c80152bd7d04673579c
Paul Furley <paul@paulfurley.com>
2013-09-12 17:20:42 +0100

    Renamed render.py to diagnostics.py and added make_annotations() which describes how to draw the result of page_to_tables

--- pdftables/diagnostics.py
+++ pdftables/diagnostics.py
@@ -5,20 +5,17 @@ from collections import namedtuple
 import patched_poppler as poppler
 import cairo
 
-from os.path import abspath, basename
+from os.path import abspath
 
 Point = namedtuple('Point', ['x', 'y'])
-Line = namedtuple('Line', ['start', 'end'])
+Line = namedtuple('Line', ['orientation', 'edge'])
 Rectangle = namedtuple('Rectangle', ['top_left', 'bottom_right'])
 Annotation = namedtuple('Annotation', ['name', 'colour', 'shapes'])
 Colour = namedtuple('Colour', ['red', 'green', 'blue'])
 
 __all__ = [
-    'render_annotated_pdf_page',
-    'Point',
-    'Line',
-    'Rectangle',
-    'Annotation',
+    'render_page',
+    'make_annotations',
 ]
 
 
@@ -82,7 +79,7 @@ class CairoPdfPageRenderer(object):
         self._surface.finish()
 
 
-def render_annotated_pdf_page(pdf_filename, page_number,
+def render_page(pdf_filename, page_number,
                               annotations, svg_file=None,
                               png_file=None):
     """
@@ -110,6 +107,54 @@ def extract_pdf_page(filename, page_number):
     return page
 
 
+def make_annotations(table_container):
+    """
+    Take the output of the table-finding algorithm (TableFinder) and create
+    Annotations. These can be drawn on top of the original PDF page to
+    visualise how the algorithm arrived at its output.
+    """
+
+    annotations = []
+
+    annotations.append(
+        Annotation(
+            name='table_bounding_boxes',
+            colour=(1, 0, 0),
+            shapes=convert_rectangles(table_container.bounding_boxes)))
+
+    annotations.append(
+        Annotation(
+            name='all_glyphs',
+            colour=(0, 1, 0),
+            shapes=convert_rectangles(table_container.all_glyphs)))
+
+    for table in table_container:
+        annotations.append(
+            Annotation(
+                name='row_edges',
+                colour=(0, 0, 1),
+                shapes=convert_horizontal_lines(table.row_edges)))
+
+        annotations.extend(
+            Annotation(
+                name='column_edges',
+                colour=(0, 0, 1),
+                shapes=convert_vertical_lines(table.column_edges)))
+
+    return annotations
+
+
+def convert_rectangles(boxes):
+    return [Rectangle(Point(b.left, b.top), Point(b.bottom, b.right)) for b
+            in boxes]
+
+
+def convert_horizontal_lines(edges):
+    return [Line('horizontal', edge) for edge in edges]
+
+
+def convert_vertical_lines(edges):
+    return [Line('vertical', edge) for edge in edges]
 if __name__ == '__main__':
     annotations = [
         Annotation(
@@ -117,4 +162,4 @@ if __name__ == '__main__':
             colour=Colour(1, 0, 0),
             shapes=[Rectangle(Point(100, 100), Point(200, 200))])
     ]
-    render_annotated_pdf_page(sys.argv[1], 0, annotations)
+    render_page(sys.argv[1], 0, annotations)
--- pdftables/scripts/dump.py
+++ pdftables/scripts/dump.py
@@ -19,11 +19,12 @@ import pdftables
 # Use $ pip install --editable pdftables
 # to install this util in your path.
 
-from pdftables.pdf_document import PDFDocument
-from pdftables.render import render_annotated_pdf_page
 from os.path import basename
-
 from docopt import docopt
+
+from pdftables.pdf_document import PDFDocument
+from pdftables.diagnostics import render_page, make_annotations
+from pdftables.pdftables import page_to_tables
 
 
 def main():
@@ -35,19 +36,21 @@ def main():
     if arguments["--check"]:
         return check(arguments["<pdfpath>"][0])
 
+    for pdf_filename in arguments["<pdfpath>"]:
+        with open(pdf_filename, "rb") as fd:
 
-    for pdfpath in arguments["<pdfpath>"]:
-        with open(pdfpath, "rb") as fd:
             doc = PDFDocument.from_fileobj(fd)
             print doc
             for page_number, page in enumerate(doc.get_pages()):
                 svg_file = 'svgs/{0}_{1}.svg'.format(
-                    basename(pdfpath), page_number)
+                    basename(pdf_filename), page_number)
                 png_file = 'pngs/{0}_{1}.png'.format(
-                    basename(pdfpath), page_number)
-                render_annotated_pdf_page(pdfpath, page_number, [],
-                                          svg_file, png_file)
+                    basename(pdf_filename), page_number)
+                table_container = page_to_tables(page)
+                annotations = make_annotations(table_container)
 
+                render_page(
+                    pdf_filename, page_number, annotations, svg_file, png_file)
 
 def check(path):
     fileobj = open(path, 'rb')

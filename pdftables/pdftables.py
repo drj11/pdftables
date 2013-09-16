3cf41f54be5c3873b77aa11e3cf55c4bfb3cbcaa
Peter Waller <peter@scraperwiki.com>
2013-09-16 19:26:09 +0100

    Remove functions which are now unused

--- pdftables/pdftables.py
+++ pdftables/pdftables.py
@@ -106,19 +106,6 @@ def page_contains_tables(pdf_page):
     return len(test) > IS_TABLE_ROW_COUNT_THRESHOLD
 
 
-def threshold_above(hist, threshold_value):
-    """
-    >>> threshold_above(Counter({518: 10, 520: 20, 530: 20, \
-                                             525: 17}), 15)
-    [520, 530, 525]
-    """
-    if not isinstance(hist, Counter):
-        raise ValueError("requires Counter")  # TypeError then?
-
-    above = [k for k, v in hist.items() if v > threshold_value]
-    return above
-
-
 def apply_combs(box_list, x_comb, y_comb):
     """
     Allocates text to table cells using the x and y combs
@@ -146,200 +133,6 @@ def apply_combs(box_list, x_comb, y_comb):
         table_array[row][col] += box.text.rstrip('\n\r')
 
     return table_array
-
-
-def comb(combarray, value):
-    """
-    Takes a sorted array and returns the interval number of the value passed to
-    the function
-    """
-    # Raise an error in combarray not sorted
-    if (combarray != sorted(combarray)) and (combarray != sorted(
-            combarray, reverse=True)):
-        raise Exception("comb: combarray is not sorted")
-
-    index = -1
-    if combarray[0] > combarray[-1]:
-        for i in range(1, len(combarray)):
-            if combarray[i - 1] >= value >= combarray[i]:
-                index = i - 1
-    else:
-        for i in range(1, len(combarray)):
-            if combarray[i - 1] <= value <= combarray[i]:
-                index = i - 1
-
-    return index
-
-
-def apply_combs_old(box_list, x_comb, y_comb):
-    """Allocates text to table cells using the x and y combs"""
-    ncolumns = len(x_comb) - 1
-    nrows = len(y_comb) - 1
-    table_array = [[''] * ncolumns for j in range(nrows)]
-    for box in box_list:
-        y = round(box.midline)
-        x = round(box.centreline)
-        rowindex = comb(y_comb, y)
-        columnindex = comb(x_comb, x)
-        if rowindex != -1 and columnindex != -1:
-            # there was already some content at this coordinate so we
-            # concatenate (in an arbitrary order!)
-            table_array[rowindex][columnindex] += box.text.rstrip('\n\r')
-
-    return table_array
-
-
-def comb_from_projection(projection, threshold, orientation):
-    """Calculates the boundaries between cells from the projection of the boxes
-    onto either the y axis (for rows) or the x-axis (for columns). These
-    boundaries are known as the comb
-    """
-    if orientation == "row":
-        tol = 1
-    elif orientation == "column":
-        tol = 3
-
-    projection_threshold = threshold_above(projection, threshold)
-
-    projection_threshold = sorted(projection_threshold)
-    # need to generate a list of uppers (right or top edges)
-    # and a list of lowers (left or bottom edges)
-
-    # uppers = [k for k, v in yhisttop.items() if v > yThreshold]
-    # lowers = [k for k, v in yhistbottom.items() if v > yThreshold]
-    uppers = []
-    lowers = []
-
-    # TODO(pwaller): Use a more readable algorithm, determine purpose, maybe
-    #                refactor or use more general approach.
-    #
-    # What is the + 1 for? Why?
-    #
-    # for this, next in zip(a, a[1:])
-    #     if this + 1 < next:
-    #         uppers.append(this)
-    #         lowers.append(next)
-
-    lowers.append(projection_threshold[0])
-    for i in range(1, len(projection_threshold)):
-        if projection_threshold[i] > (
-                projection_threshold[i - 1] + 1):
-            uppers.append(projection_threshold[i - 1])
-            lowers.append(projection_threshold[i])
-    uppers.append(projection_threshold[-1])
-
-    comb = comb_from_uppers_and_lowers(uppers, lowers, tol=tol,
-                                       projection=projection)
-    comb.reverse()
-
-    return comb
-
-
-def comb_from_uppers_and_lowers(uppers, lowers, tol=1, projection=dict()):
-    """
-    Called by comb_from_projection to calculate the comb given a set of
-    uppers and lowers, which are upper and lower edges of the thresholded
-    projection
-    """
-
-    # tol is a tolerance to remove very small minima, increasing to 2 fowls up
-    # row separation
-    assert len(uppers) == len(lowers)
-    uppers.sort(reverse=True)
-    lowers.sort(reverse=True)
-    comb = []
-    comb.append(uppers[0])
-    for i in range(1, len(uppers)):
-        if (lowers[i - 1] - uppers[i]) > tol:
-            comb.append(find_minima(lowers[i - 1], uppers[i], projection))
-            #comb.append(find_minima(lowers[i - 1], uppers[i]))
-
-    comb.append(lowers[-1])
-
-    return comb
-
-
-def find_minima(lower, upper, projection=dict()):
-
-    # print lower, upper, projection
-    if len(projection) == 0:
-        idx = (lower + upper) / 2.0
-    else:
-        profile = []
-        for i in range(upper, lower):
-            # print projection[i]
-            profile.append(projection[i])
-
-        val, idx = min((val, idx) for (idx, val) in enumerate(profile))
-        #val, idx = min(profile)
-        idx = upper + idx
-
-    return idx
-
-
-def comb_extend(comb, minv, maxv):
-    """
-    Extend the comb to minv and maxv
-    """
-    # TODO should this truncate if minv>minc or maxc>maxc
-    # print y_comb
-    # Find sort order of comb, convert to ascending
-    reversed = False
-    if comb[0] > comb[-1]:
-        comb.reverse()
-        reversed = True
-
-    # Find min and max of comb
-    minc = comb[0]
-    maxc = comb[-1]
-
-    # Get average row spacing
-    rowSpacing = numpy_subset.average(numpy_subset.diff(comb))
-
-    # Extend minimum
-    if minv < minc:
-        comb.reverse()
-        comb.extend(list(numpy_subset.arange(minc, minv, -rowSpacing))[1:])
-        comb.reverse()
-
-    # Extend maximum
-    if maxv > maxc:
-        comb.extend(list(numpy_subset.arange(maxc, maxv, rowSpacing))[1:])
-
-    if reversed:
-        comb.reverse()
-    return comb
-
-
-def project_boxes(box_list, orientation, erosion=0):
-    """
-    Take a set of boxes and project their extent onto an axis
-    """
-    if orientation == "column":
-        first = attrgetter("left")
-        second = attrgetter("right")
-    elif orientation == "row":
-        first = attrgetter("top")
-        second = attrgetter("bottom")
-
-    projection = {}
-    # ensure some overlap
-    minv = round(min([first(box) for box in box_list])) - 2
-    maxv = round(max([second(box) for box in box_list])) + 2
-
-    # Initialise projection structure
-    # print minv, maxv
-    coords = range(int(minv), int(maxv))
-    projection = coords
-
-    # print projection
-    for box in box_list:
-        for i in range(int(round(first(box))) + erosion,
-                       int(round(second(box))) - erosion):
-            # projection[i] += 1
-            projection.append(i)
-
-    return Counter(projection)
 
 
 class Table(object):
@@ -495,32 +288,6 @@ def compute_cell_edges(box, h_segments, v_segments, config):
 
     return column_edges, row_edges
 
-    # Project boxes onto horizontal axis
-    column_projection = project_boxes(box_list, "column")
-
-    # Project boxes onto vertical axis
-    # Erode row height by a fraction of the modal text box height
-    erodelevel = int(math.floor(calculate_modal_height(box_list) / 4))
-    row_projection = project_boxes(
-        box_list, "row",
-        erosion=erodelevel)
-
-    rowThreshold = 3
-    y_comb = comb_from_projection(row_projection, rowThreshold, "row")
-    y_comb.reverse()
-
-    columnThreshold = 5  # 3 works for smaller tables
-    x_comb = comb_from_projection(column_projection, columnThreshold, "column")
-
-    x_comb[0] = bounds.left
-    x_comb[-1] = bounds.right
-
-    # Extend y_comb to page size if extend_y is true
-    if config.extend_y:
-        y_comb = comb_extend(y_comb, bounds.top, bounds.bottom)
-
-    return x_comb, y_comb
-
 
 def compute_table_data(table):
     """
@@ -546,6 +313,18 @@ def find_table_bounding_box(box_list, table_top_hint, table_bottom_hint):
     # TODO(pwaller): These closures are here to make it clear how these things
     #                belong together. At some point it may get broken apart
     #                again, or simplified.
+
+    def threshold_above(hist, threshold_value):
+        """
+        >>> threshold_above(Counter({518: 10, 520: 20, 530: 20, \
+                                                 525: 17}), 15)
+        [520, 530, 525]
+        """
+        if not isinstance(hist, Counter):
+            raise ValueError("requires Counter")  # TypeError then?
+
+        above = [k for k, v in hist.items() if v > threshold_value]
+        return above
 
     def threshold_y():
         """
@@ -585,10 +364,12 @@ def find_table_bounding_box(box_list, table_top_hint, table_bottom_hint):
             top_box = [box for box in glyphs if table_top_hint in box.text]
             if top_box:
                 miny = top_box[0].top
+
         if table_bottom_hint:
             bottomBox = [box for box in glyphs if table_bottom_hint in box.text]
             if bottomBox:
                 maxy = bottomBox[0].bottom
+
         return Box(Rectangle(
             x1=float("-inf"),
             y1=miny,
@@ -601,12 +382,3 @@ def find_table_bounding_box(box_list, table_top_hint, table_bottom_hint):
     hinted_bounds = hints_y()
 
     return bounds.clip(threshold_bounds, hinted_bounds)
-
-
-def calculate_modal_height(box_list):
-    height_list = []
-    for box in box_list:
-        height_list.append(round(box.bottom - box.top))
-
-    modal_height = Counter(height_list).most_common(1)
-    return modal_height[0][0]
